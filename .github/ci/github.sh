37fbb9fba5b21055a5e2d02e2bae2c6bb0a25a73
franklin selva <franklinselva10@gmail.com>
2023-01-09 09:28:00 +0100

    fix executable name

--- .github/ci/github.sh
+++ .github/ci/github.sh
@@ -4,6 +4,7 @@
 dir_name=up_aries/bins
 arch=$1
 distro=$2
+os=$3
 
 # Get the list of modified files
 modified_files=$(git diff-tree --no-commit-id --name-only -r HEAD)
@@ -11,10 +12,10 @@ modified_files=$(git diff-tree --no-commit-id --name-only -r HEAD)
 # Check if the directory was modified
 if echo "$modified_files" | grep -q "$dir_name"; then
     # Directory was modified, so push the changes
-    git add up_aries/bins/aries-linux-"${arch}"
+    git add up_aries/bins/aries-"${os}"-"${arch}"
     git config --global user.name "github actions"
     git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
-    git commit -m "update aries binary for ${distro}-${arch}"
+    git commit -m "update aries binary for ${os}-${arch}"
     git pull --rebase
     git push
 else
